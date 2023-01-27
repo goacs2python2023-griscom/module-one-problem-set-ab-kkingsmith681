7ffc08d154225cb629cd4f1795942c07594cbb07
kkingsmith681 <kkingsmith@rundlecollege.ca>
2023-01-27 15:44:11 -0700

    Created square.py program

---
+++ square.py
@@ -0,0 +1,8 @@
+'''Write a function that takes one number as an argument and returns the square of that number.'''
+
+x = int(input("What number do you want to square? "))
+
+def square2(x):
+    return x*x
+
+print(square2(x))
